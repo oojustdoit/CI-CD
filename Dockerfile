e404a2f50295b0f08d046143b3d8732aa6f28fa3
devops-user <devops@example.com>
2026-06-22 20:10:07 +0800

    test ci/cd

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ FROM python:3.10-slim
 WORKDIR /app
 
 COPY app/ /app
+RUN pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --no-cache-dir -r requirements.txt
 
-RUN pip install --no-cache-dir -r requirements.txt
+EXPOSE 5000
 
 CMD ["gunicorn", "-w", "2", "-b", "0.0.0.0:5000", "app:app"]
--- app/app.py
+++ app/app.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask, Response
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "CI/CD SUCCESS c2!"
+# =========================
+# 📊 Prometheus 指标
+# =========================
+REQUEST_COUNT = Counter(
+    'request_count',
+    'Total request count of the app'
+)
 
+# =========================
+# 🌐 主接口
+# =========================
+@app.route("/")
+def hello():
+    REQUEST_COUNT.inc()  # 每次访问 +1
+    return "Hello CI/CD + Monitoring 🚀"
+
+# =========================
+# 📈 Prometheus 监控接口
+# =========================
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+
+# =========================
+# 🚀 启动入口
+# =========================
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
